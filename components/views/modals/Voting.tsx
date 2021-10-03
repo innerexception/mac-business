b1aa77a9265cb8f0e7d976ed1958b9c3429dc575
jbaker <jbakerdev@gmail.com>
2021-10-03 15:27:29 -0400

    votes

--- components/views/modals/Voting.tsx
+++ components/views/modals/Voting.tsx
@@ -20,12 +20,13 @@ export default class Voting extends React.PureComponent<Props> {
                 <h4 style={{marginBottom:'1em'}}>Your Vote Matters!</h4>
                 <h5 style={{marginBottom:'1em'}}>Tell us how we can improve.</h5>
                 <h5 style={{marginBottom:'1em'}}>Your votes: {this.props.me.votes}</h5>
-                {EdictArray.map(e=>
-                    <div style={{marginBottom:'1em'}}>
-                        <h5>{Edicts[e].name}</h5>
-                        <h6 style={{marginBottom:'0.5em'}}>{Edicts[e].description}</h6>
-                        {Button(!this.props.me.pendingVote, ()=>Provider.onSubmitVotes({edict:e, playerId:this.props.me.uid}), 'Vote Up ->')}
-                    </div>
+                {this.props.me.pendingVote ? <h5>You voted for {Edicts[this.props.me.pendingVote].name}.</h5> :
+                    EdictArray.map(e=>
+                        <div style={{marginBottom:'1em'}}>
+                            <h5>{Edicts[e].name}</h5>
+                            <h6 style={{marginBottom:'0.5em'}}>{Edicts[e].description}</h6>
+                            {Button(!this.props.me.pendingVote, ()=>Provider.onSubmitVotes({edict:e, playerId:this.props.me.uid}), 'Vote Up ->')}
+                        </div>
                 )}
             </div>
         )
