79b0008e900e57f6d4357b317c810dde7e5d9f82
jbaker <jbakerdev@gmail.com>
2021-10-03 15:23:56 -0400

    votes

--- components/views/modals/Voting.tsx
+++ components/views/modals/Voting.tsx
@@ -17,12 +17,14 @@ export default class Voting extends React.PureComponent<Props> {
     render(){
         return (
             <div style={{...AppStyles.modal, ...AppStyles.centered}}>
-                <h4>Your votes: {this.props.me.votes}</h4>
+                <h4 style={{marginBottom:'1em'}}>Your Vote Matters!</h4>
+                <h5 style={{marginBottom:'1em'}}>Tell us how we can improve.</h5>
+                <h5 style={{marginBottom:'1em'}}>Your votes: {this.props.me.votes}</h5>
                 {EdictArray.map(e=>
-                    <div style={{width:'100px'}}>
+                    <div style={{marginBottom:'1em'}}>
                         <h5>{Edicts[e].name}</h5>
-                        <h6>{Edicts[e].description}</h6>
-                        {Button(!this.props.me.pendingVote, ()=>Provider.onSubmitVotes({edict:e, playerId:this.props.me.uid}), 'Make it so ->')}
+                        <h6 style={{marginBottom:'0.5em'}}>{Edicts[e].description}</h6>
+                        {Button(!this.props.me.pendingVote, ()=>Provider.onSubmitVotes({edict:e, playerId:this.props.me.uid}), 'Vote Up ->')}
                     </div>
                 )}
             </div>
