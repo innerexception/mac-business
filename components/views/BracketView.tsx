3c8ad452f4b8af4bbe3b4d30d9f489fb09bbb7d2
jbaker <jbakerdev@gmail.com>
2021-10-03 14:27:01 -0400

    Wagers

--- components/views/BracketView.tsx
+++ components/views/BracketView.tsx
@@ -11,17 +11,11 @@ interface Props {
     round:number
 }
 
-interface State {
-    wager:number
-}
-
 @(connect((state: RState) => ({
     me: state.onlineAccount,
     match: state.tournament
 })) as any)
-export default class BracketView extends React.PureComponent<Props, State> {
-
-    state:State = { wager: 0 }
+export default class BracketView extends React.PureComponent<Props> {
 
     render(){
         const brackets = this.props.match.brackets.filter(b=>b.round === this.props.round)
@@ -31,21 +25,19 @@ export default class BracketView extends React.PureComponent<Props, State> {
             <div>
                 {brackets.map(b=>
                 <div style={{padding:'5px', border:'1px '+(this.props.round === this.props.match.activeRound ? 'dashed':'solid'), marginBottom:'1em'}}>
-                    <Bracket playerId={b.player1Id} myId={this.props.me.uid}/>
-                    {!isParticipant && b.player1Id &&
-                    <div>
-                        {NumericInput(this.state.wager, (val)=>this.setState({wager: val}), this.props.me.votes, 0)}
-                        {Button(true, ()=>Provider.onSubmitWager({ amount: this.state.wager, bracketId: b.uid, playerToWin: b.player1Id}), 'Place Bet')}
-                    </div>
-                    }
+                    <Bracket 
+                        isParticipant={isParticipant}
+                        bracketId={b.uid}
+                        playerId={b.player1Id} 
+                        wager={this.props.me.wagers.find(w=>w.bracketId === b.uid && w.playerToWin === b.player1Id)}
+                        myId={this.props.me.uid}/>
                     <h5 style={{textAlign:'center', margin:'0.5em'}}>VS</h5>
-                    <Bracket playerId={b.player2Id} myId={this.props.me.uid}/>
-                    {!isParticipant && b.player2Id &&
-                    <div>
-                        {NumericInput(this.state.wager, (val)=>this.setState({wager: val}), this.props.me.votes, 0)}
-                        {Button(true, ()=>Provider.onSubmitWager({ amount: this.state.wager, bracketId: b.uid, playerToWin: b.player2Id}), 'Place Bet')}
-                    </div>
-                    }
+                    <Bracket 
+                        isParticipant={isParticipant}
+                        bracketId={b.uid}
+                        playerId={b.player2Id} 
+                        wager={this.props.me.wagers.find(w=>w.bracketId === b.uid && w.playerToWin === b.player2Id)}
+                        myId={this.props.me.uid}/>
                 </div>
                 )}
             </div>
